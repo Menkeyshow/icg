89bac0b5e07f1a6168b81a87396502f61f4663b3
Menkeyshow <darkwolve.darkwolve@gmail.com>
2018-01-29 17:06:54 +0100

    bla, shits not working yo

--- Übung 8/Uebung 7 - Birkenhagen_Geislinger_Regorz_Schoenfeldt.js
+++ Übung 8/Uebung 7 - Birkenhagen_Geislinger_Regorz_Schoenfeldt.js
@@ -1081,6 +1081,12 @@ function init() {
 	target = vec3.fromValues(0.0, -0.65, 0.0);
 	up = vec3.fromValues(0.0, 1.0, 0.0);
 
+	/*
+	eye = vec3.fromValues(0.0, -0.0, 0.00);
+	target = vec3.fromValues(0.0, -0.1, 0.0);
+	up = vec3.fromValues(1.0, 0.0, 0.0);
+	*/
+
 	viewMatrix = mat4.create();
 	mat4.lookAt(viewMatrix, eye, target, up);
 
